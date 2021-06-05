0ef456ed0183702ee841a3537c6a606edc56adf2
Meet Master <meetmaster16@gmail.com>
2021-06-05 20:35:34 +0530

    third commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,12 @@ const About = () => {
   return (
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
-      <p>know your favorite cocktail and get their ingredients!</p>
+      <p>
+        Cocktail DB is your one stop drinktionary! We're here to take your
+        cocktail experience beyond the idea of a simple drink. From the
+        ingredients to the alcohol content, right from the bartender to the
+        final serving, we've it all covered!
+      </p>
     </section>
   );
 };
