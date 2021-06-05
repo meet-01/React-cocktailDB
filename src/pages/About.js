a03791f482a1102343271066aac00ab807929474
Meet Master <meetmaster16@gmail.com>
2021-06-05 20:25:26 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>know your favorite cocktail and get their ingredients!</p>
+    </section>
+  );
+};
+
+export default About;
